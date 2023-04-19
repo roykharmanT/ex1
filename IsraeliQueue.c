6b93dc2fa3d80e4b9ab8273d5be23d387bb45401
ShohamKalman <shohamk31@gmail.com>
2023-04-19 15:36:26 +0300

    added coma

--- IsraeliQueue.c
+++ IsraeliQueue.c
@@ -1,24 +1,27 @@
 #include "IsraeliQueue.h"
-typedef struct IsraeliQueue_t * IsraeliQueue{
-        FriendshipFunction *friend_function;
-        ComparisonFunction comparison_function;
-        int rivalry_threshold;
-        int friendship_threshold;
-        item *head;
-}*IsraeliQueue;
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct item{
     int friends_in_queue;
     int rivals_in_queue;
-    item* next;
+    struct item* next;
 }*Item;
+
+typedef struct IsraeliQueue_t{
+        FriendshipFunction *friend_function;
+        ComparisonFunction comparison_function;
+        int rivalry_threshold;
+        int friendship_threshold;
+        Item head;
+}*IsraeliQueue;
 
 
 IsraeliQueue IsraeliQueueClone(IsraeliQueue q)
 {
     if(q==NULL)
         return NULL;
-    IsraeliQueue new_queue = IsraeliQueueCreate(q->friend_function, q->comparison_function
+    IsraeliQueue new_queue = IsraeliQueueCreate(q->friend_function, q->comparison_function,
     q->rivalry_threshold, q->friendship_threshold);
     return new_queue;
 }
